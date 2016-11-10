7187fe9ddb3049b42ffe02429cc1b0fa60f06dc6
Justin Thomas <jthomas0209@gmail.com>
2016-11-10 12:53:46 -0500

    Done

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,11 @@
 
 
 describe('index', function() {
-  it('runs', function() {
-    expect(true).toEqual(true)
-  })
-})
+  it('should return a cloned heroes array with newly added intials key', function() {
+		let expected = [{ firstName: 'Tony', lastName: 'Stark', heroName: 'Iron Man', initials:'TS' },
+  									{ firstName: 'Steve', lastName: 'Rogers', heroName: 'Captain America', initials: 'SR' },
+  									{ firstName: 'Barry', lastName: 'Allen', heroName: 'The Flash', initials: 'BA' }
+									 ];
+    expect(addInitialsToHeroes(heroes)).toMatch(expected);
+  });
+});
